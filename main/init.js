cd56ef9cd73faa0ac72a42fdb3bc6dd9e1edbf9e
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-06-15 20:44:29 +0100

    Removed commented out code.

--- main/init.js
+++ main/init.js
@@ -1,10 +1,6 @@
 //---===~ Add listeners. ~===-------------------------------------------------------------------------------------------
 chrome.windows.onCreated.addListener(windowsOnCreatedListener);
 chrome.windows.onRemoved.addListener(windowsOnRemovedListener);
-// Cannot use windows.onRemoved because window has already removed when the event fires, so cannot get tab info and
-// therefore cannot save the session.
-// Causing lots of problems with window id for non existing windows being passed.
-// chrome.tabs.onUpdated.addListener(tabsOnUpdatedListener);
 chrome.browserAction.onClicked.addListener(browserActionOnClickedListener);
 
 function windowsOnCreatedListener(windowToCheck) {
@@ -13,10 +9,6 @@ function windowsOnCreatedListener(windowToCheck) {
 
 function windowsOnRemovedListener(windowId) {
   removeWindowToSessionFolderMapping(windowId);
-}
-
-function tabsOnUpdatedListener(windowToCheckId) {
-  saveSession(windowToCheckId);
 }
 
 function browserActionOnClickedListener(tab) {
