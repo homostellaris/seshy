fcd4f697083c86644a38b3e2ae20ae0c4fde0008
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-06-25 09:22:57 +0100

    Added draft session manager page.
    The session manager is accessed via the extension's browser action (clicking on the extension button) and will allow users to save and restore there sessions among other things.
    Material design web components are used to keep the style coherent with Chrome and other Google products.

--- main/session-manager.js
+++ main/session-manager.js
@@ -8,15 +8,24 @@ function appendSessions(sessionFolders) {
   for (var i = 0; i < sessionFolders.length; i++) {
     var sessionFolder = sessionFolders[i];
 
-    var sessionElement = document.createElement('div');
-    sessionElement.setAttribute('class', 'session');
-    sessionElement.innerHTML = '<h3>' + sessionFolder.title + '</h3>' +
-    '<h4>' + sessionFolder.children.length + ' tabs' + '</h4>' +
-    '<p class="train-of-thought">' +
-    "Need to add deletion of sessions. Explore using Google's material design repositories on GitHub for style." +
-    "Remember that the extension should look and feel like a natural extension of Chrome." +
-    '</p>';
+    var sessionList = document.getElementById('saved-sessions-list');
+    var sessionElement = document.createElement('li');
+    sessionElement.setAttribute('class', 'session mdc-list-item mdc-theme--background mdc-elevation--z2');
+    sessionElement.innerHTML = '<span class="mdc-list-item__start-detail">' +
+    '<i class="material-icons">backup</i>' +
+    '</span>' +
+    '<span class="mdc-list-item__text">' +
+    sessionFolder.title +
+    '<span class="mdc-list-item__text__secondary">' +
+    sessionFolder.children.length + ' tabs' +
+    '</span>' +
+    '</span>' +
+    '<span class="mdc-list-item__end-detail">' +
+    '<i class="material-icons">delete</i>' +
+    '</span>';
 
-    container.appendChild(sessionElement);
+    sessionList.appendChild(sessionElement);
   }
 }
+
+mdc.autoInit();
