e569c96880a9da8f9aeff68c71c5d15f04494a3f
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-10-07 21:28:47 +0100

    Added skeleton spec to be implemented.

--- test/spec/session-manager.js
+++ test/spec/session-manager.js
@@ -1,1 +1,36 @@
-// Not implemented!
+describe('Session selection.', () => {
+  xit('Creates a blue border around the currently selected session.', () => {
+    
+
+  })
+
+  xit('Creates an orange border around the currently open session.', () => {
+
+  })
+
+  xit('Selects the currently open session when opened.', () => {
+
+  })
+
+  describe('Hotkeys.', () => {
+    xit('Selects the session above the currently selected one when the `UP` arrow key is pressed.', () => {
+
+    })
+
+    xit('Selects the session above the currently selected one when the `UP` arrow key is pressed.', () => {
+
+    })
+
+    xit('Selects the session below the currently selected one when the `DOWN` arrow key is pressed.', () => {
+
+    })
+
+    xit('Selects the last session in the previous section when the `LEFT` arrow key is pressed.', () => {
+
+    })
+
+    xit('Selects the first session in the next section when the `RIGHT` arrow key is pressed.', () => {
+
+    })
+  })
+})
