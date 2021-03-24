b22b121f8a4f1abdb047a8b6bcbf1cb6a6e7286b
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2021-03-24 21:12:29 +0000

    Finally have CSS bundled and working

--- snowpack.config.cjs
+++ snowpack.config.cjs
@@ -20,5 +20,10 @@ module.exports = {
   },
   optimize: {
     bundle: true,
+    entrypoints: [ // TODO: See if 'auto' starts working later so this can be removed.
+      'worker.js',
+      'index.js', // TODO: See if CSS bundling starts working with only specific parts of the app bundled.
+    ],
+    sourcemap: false,
   },
 };
