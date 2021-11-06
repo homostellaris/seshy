b1a4b7672a439949dbbd662f7d13642a488f07ed
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2021-11-06 15:13:01 +0000

    Add error handling to publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -8,22 +8,42 @@ import chromeWebstoreUpload from 'chrome-webstore-upload'
 import fs from 'fs'
 import zipdir from 'zip-dir'
 
-const dir = './dist';
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-}
-
-await zipdir('./build', { saveTo: './dist/seshy.zip' });
+createDistFolder();
+await zipBuildFolder();
 
 const store = chromeWebstoreUpload({
 	extensionId: 'noeieddjehppejohbbchbcmheecaneac',
 	clientId: process.env.SESHY_PUBLISHER_CLIENT_ID,
 	refreshToken: process.env.SESHY_PUBLISHER_REFRESH_TOKEN,
 })
-const token = await store.fetchToken()
-const myZipFile = fs.createReadStream('./dist/seshy.zip')
-const resource = await store.uploadExisting(myZipFile, token)
-console.info('New version uploaded', resource)
 
-await store.publish('default', token)
-console.info('New version published')
+try {
+	const token = await store.fetchToken()
+	await uploadZip(token);
+	await publish(token);
+} catch (error) {
+	console.error(error.message, '\n', error.response.body)
+}
+
+async function publish (token) {
+	await store.publish('default', token);
+	console.info('New version published');
+}
+
+async function uploadZip (token) {
+	const seshyZip = fs.createReadStream('./dist/seshy.zip');
+	const resource = await store.uploadExisting(seshyZip, token);
+	console.info('New version uploaded', resource);
+}
+
+async function zipBuildFolder () {
+	await zipdir('./build', { saveTo: './dist/seshy.zip' });
+}
+
+function createDistFolder () {
+	const dir = './dist';
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir);
+	}
+}
+
