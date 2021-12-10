289f05670b36890209e953bd50c99a74144f182c
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2021-12-10 22:39:22 +0000

    Update help page

--- src/ui/moreInformation/index.js
+++ src/ui/moreInformation/index.js
@@ -1,9 +1,3 @@
-function openNewTabThenExitActionPopup (url) {
-	const createProperties = {url}
-	chrome.tabs.create(createProperties)
-	window.close()
-}
-
 function submitAFeatureRequest  () {
 	openNewTabThenExitActionPopup('https://github.com/moderatemisbehaviour/seshy/discussions/categories/ideas')
 }
@@ -12,18 +6,24 @@ function reportABug  () {
 	openNewTabThenExitActionPopup('https://github.com/moderatemisbehaviour/seshy/issues?q=is%3Aopen+is%3Aissue+label%3Abug')
 }
 
-function seeOpenSourceCredits () {
-	window.location.href = '../credits/index.html'
+function leaveAReview  () {
+	openNewTabThenExitActionPopup('https://chrome.google.com/webstore/detail/seshy/noeieddjehppejohbbchbcmheecaneac/related?hl=en-GB')
+}
+
+function openNewTabThenExitActionPopup (url) {
+	const createProperties = {url}
+	chrome.tabs.create(createProperties)
+	window.close()
 }
 
 function setUp () {
 	const submitAFeatureRequestElement = document.getElementById('submit-a-feature-request')
 	const reportABugElement = document.getElementById('report-a-bug')
-	const seeOpenSourceCreditsElement = document.getElementById('see-open-source-credits')
+	const leaveAReviewElement = document.getElementById('leave-a-review')
 
 	submitAFeatureRequestElement.addEventListener('click', submitAFeatureRequest)
 	reportABugElement.addEventListener('click', reportABug)
-	seeOpenSourceCreditsElement.addEventListener('click', seeOpenSourceCredits)
+	leaveAReviewElement.addEventListener('click', leaveAReview)
 }
 
 setUp()
